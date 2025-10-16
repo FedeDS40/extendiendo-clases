823bfff9439bd46a1664d954ce1ef0e3aaf7a80b
Federico <federicodellasavia@gmail.com>
2025-10-16 07:45:36 -0300

    Primer commit del desafio 5

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import products from './products.json'
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -8,9 +9,14 @@ class ListaDeCosas {
   add(nuevaCosa) {
     this.cosas.push(nuevaCosa);
   }
-  getCosas() {
-    return this.cosas;
-  }
+getCosas() {
+  return this.cosas.map((p) => ({
+    name: p.name,
+    price: p.price,
+    id: p.id,
+  }));
+}
+
 }
 
 class Product {
@@ -24,6 +30,40 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  nameLista:string
+  constructor(name:string){
+    super(name);
+    const productos=require('./products.json');
+    console.log(productos)
+    productos.forEach(producto => {
+      const nuevoPrducto=new Product(producto.name,producto.price,producto.id);
+      this.addProduct(nuevoPrducto);
+      
+    });
+
+  }
+  addProduct(producto:Product):void{
+    const exist=this.cosas.some(existeProducto=>existeProducto.id===producto.id);
+    if(!exist){
+      this.cosas.push(producto)
+    }else{
+      console.error(`Este producto: ${producto}, ya existe`);
+    }
+  }
+      getProduct(id:number):Product | undefined{
+        return(this.cosas.find(product=>product.id===id));
+    }
+    removeProduct(id:number):void{
+      this.cosas=this.cosas.filter(product=>product.id!==id);
+      }
+    getSortedByPrice(order:string):Product[]{
+      let ordenado=[...this.cosas].sort((a,b)=>a.price-b.price);
+      if(order==="desc"){
+        return ordenado.reverse();
+      }
+      return ordenado;
+    }
+}
 
 export { ListaDeProductos, Product };
